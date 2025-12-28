e078e780036b66f56f8b7a46ef0a36db055694e8
Ieuan Walker <ieuan.walker007@gmail.com>
2025-12-28 17:27:58 +0000

    Add GlobalSuppressions.cs to suppress RS2001 warnings
    Added GlobalSuppressions.cs with assembly-level SuppressMessage attributes to suppress RS2001 analyzer warnings for specific fields in the Helpers namespace. Suppressions are scoped to HTTP verb and MapGroup helper members and marked as "<Pending>" for justification.

---
+++ src/IeuanWalker.MinimalApi.Endpoints.Generator/GlobalSuppressions.cs
@@ -0,0 +1,12 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("MicrosoftCodeAnalysisReleaseTracking", "RS2001:Ensure up-to-date entry for analyzer diagnostic IDs are added to analyzer release", Justification = "<Pending>", Scope = "member", Target = "~F:IeuanWalker.MinimalApi.Endpoints.Generator.Helpers.HttpVerbRouteHelpers.multipleHttpVerbsDescriptor")]
+[assembly: SuppressMessage("MicrosoftCodeAnalysisReleaseTracking", "RS2001:Ensure up-to-date entry for analyzer diagnostic IDs are added to analyzer release", Justification = "<Pending>", Scope = "member", Target = "~F:IeuanWalker.MinimalApi.Endpoints.Generator.Helpers.HttpVerbRouteHelpers.noHttpVerbDescriptor")]
+[assembly: SuppressMessage("MicrosoftCodeAnalysisReleaseTracking", "RS2001:Ensure up-to-date entry for analyzer diagnostic IDs are added to analyzer release", Justification = "<Pending>", Scope = "member", Target = "~F:IeuanWalker.MinimalApi.Endpoints.Generator.Helpers.MapGroupHelpers.multipleGroupCallsDescriptor")]
+[assembly: SuppressMessage("MicrosoftCodeAnalysisReleaseTracking", "RS2001:Ensure up-to-date entry for analyzer diagnostic IDs are added to analyzer release", Justification = "<Pending>", Scope = "member", Target = "~F:IeuanWalker.MinimalApi.Endpoints.Generator.Helpers.MapGroupHelpers.multipleMapGroupsDescriptor")]
+[assembly: SuppressMessage("MicrosoftCodeAnalysisReleaseTracking", "RS2001:Ensure up-to-date entry for analyzer diagnostic IDs are added to analyzer release", Justification = "<Pending>", Scope = "member", Target = "~F:IeuanWalker.MinimalApi.Endpoints.Generator.Helpers.MapGroupHelpers.noMapGroupDescriptor")]
