a0d34e3f38f381a3f231cab625a5999a9c8c3eae
Ieuan Walker <Ieuan.Walker@cardiff.gov.uk>
2026-01-07 18:35:12 +0000

    Clarify summary comment in OpenApiExtensions class
    Updated the XML summary to specify "manual rules" instead of "manually" for validation logic, improving clarity about the types of validation included in OpenAPI documentation enhancements.

--- src/IeuanWalker.MinimalApi.Endpoints/Extensions/OpenApiExtensions.cs
+++ src/IeuanWalker.MinimalApi.Endpoints/Extensions/OpenApiExtensions.cs
@@ -22,7 +22,7 @@ public static class OpenApiExtensions
 	/// <para>This enhances OpenAPI documentation for request properties, it does a number of things - </para>
 	/// <para>- Sets the appropiate types</para>
 	/// <para>- Adds in all the enum information</para>
-	/// <para>- Add validation logic (fluent validation, data annotation and manually)</para>
+	/// <para>- Add validation logic (fluent validation, data annotation and manual rules)</para>
 	/// <para>// TODO: Here is the wiki guide for more info - </para>
 	/// </summary>
 	/// <param name="options"></param>
