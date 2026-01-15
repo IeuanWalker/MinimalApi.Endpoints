b7b2e579f11db14c5b7daa855805e22a5d262bec
Ieuan Walker <Ieuan.Walker007@gmail.com>
2026-01-15 14:40:48 +0000

    Refactor tests to use Arrange/Act/Assert structure
    Refactored OpenApiSchemaHelperTests to explicitly separate Arrange, Act, and Assert sections in each test. This improves readability and maintainability without changing any test logic or behavior.

--- tests/IeuanWalker.MinimalApi.Endpoints.Tests/OpenApiDocumentTransformers/RequestPropertyEnhancer/Core/OpenApiSchemaHelperTests.cs
+++ tests/IeuanWalker.MinimalApi.Endpoints.Tests/OpenApiDocumentTransformers/RequestPropertyEnhancer/Core/OpenApiSchemaHelperTests.cs
@@ -9,10 +9,13 @@ public class OpenApiSchemaHelperTests
 	[Fact]
 	public void TryAsOpenApiSchema_ReturnsTrueForOpenApiSchema()
 	{
+		// Arrange
 		OpenApiSchema schema = new();
 
+		// Act
 		bool result = OpenApiSchemaHelper.TryAsOpenApiSchema(schema, out OpenApiSchema? openApiSchema);
 
+		// Assert
 		result.ShouldBeTrue();
 		openApiSchema.ShouldBeSameAs(schema);
 	}
@@ -22,8 +25,10 @@ public class OpenApiSchemaHelperTests
 	[InlineData(SchemaConstants.SystemInt32, JsonSchemaType.Integer, SchemaConstants.FormatInt32)]
 	public void CreatePrimitiveSchemaFromRefId_ReturnsExpected(string refId, JsonSchemaType expectedType, string? expectedFormat)
 	{
+		// Arrange + Act
 		OpenApiSchema? schema = OpenApiSchemaHelper.CreatePrimitiveSchemaFromRefId(refId);
 
+		// Assert
 		schema.ShouldNotBeNull();
 		schema!.Type.ShouldBe(expectedType);
 		schema.Format.ShouldBe(expectedFormat);
@@ -32,30 +37,39 @@ public class OpenApiSchemaHelperTests
 	[Fact]
 	public void GetTypeAndFormatFromRefId_ArrayAndCollection_ReturnsArray()
 	{
+		// Arrange + Act
 		(JsonSchemaType? t, _) = OpenApiSchemaHelper.GetTypeAndFormatFromRefId("MyNamespace.Foo[]");
+
+		// Assert
 		t.ShouldBe(JsonSchemaType.Array);
+
+		// Act
 		(t, _) = OpenApiSchemaHelper.GetTypeAndFormatFromRefId(SchemaConstants.ListGenericType + "[System.String]");
+
+		// Assert
 		t.ShouldBe(JsonSchemaType.Array);
 	}
 
 	[Fact]
 	public void CreateSchemaFromType_ArrayAndGenericAndNullable_Works()
 	{
+		// Arrange + Act
 		OpenApiSchema arraySchema = OpenApiSchemaHelper.CreateSchemaFromType(typeof(int[]));
+		OpenApiSchema listSchema = OpenApiSchemaHelper.CreateSchemaFromType(typeof(List<string>));
+		OpenApiSchema nullableSchema = OpenApiSchemaHelper.CreateSchemaFromType(typeof(int?));
+
+		// Assert
 		arraySchema.Type.ShouldBe(JsonSchemaType.Array);
 		arraySchema.Items.ShouldNotBeNull();
 		arraySchema.Items!.Type.ShouldBe(JsonSchemaType.Integer);
 
-		OpenApiSchema listSchema = OpenApiSchemaHelper.CreateSchemaFromType(typeof(List<string>));
 		listSchema.Type.ShouldBe(JsonSchemaType.Array);
 		listSchema.Items.ShouldNotBeNull();
 		listSchema.Items!.Type.ShouldBe(JsonSchemaType.String);
 
-		OpenApiSchema nullableSchema = OpenApiSchemaHelper.CreateSchemaFromType(typeof(int?));
-		// nullable int becomes a oneOf wrapper
 		nullableSchema.OneOf.ShouldNotBeNull();
 		nullableSchema.OneOf.Count.ShouldBe(2);
-		// second entry should be the nullable marker with extension
+
 		nullableSchema.OneOf[1].ShouldNotBeNull();
 		OpenApiSchema? marker = nullableSchema.OneOf[1] as OpenApiSchema;
 		marker.ShouldNotBeNull();
@@ -66,12 +80,15 @@ public class OpenApiSchemaHelperTests
 	[Fact]
 	public void UnwrapNullableEnumReference_ExtractsUnderlyingType()
 	{
+		// Arrange
 		OpenApiDocument doc = new();
 		string id = SchemaConstants.NullableTypePrefix + "[[MyNamespace.MyEnum, MyAssembly]]";
 		OpenApiSchemaReference schemaRef = new(id, doc, null);
 
+		// Act
 		OpenApiSchemaReference unwrapped = OpenApiSchemaHelper.UnwrapNullableEnumReference(schemaRef);
 
+		// Assert
 		unwrapped.Reference.ShouldNotBeNull();
 		unwrapped.Reference!.Id.ShouldBe("MyNamespace.MyEnum");
 	}
@@ -79,24 +96,26 @@ public class OpenApiSchemaHelperTests
 	[Fact]
 	public void InlinePrimitiveTypeReference_InlinesSystemTypes()
 	{
+		// Arrange
 		OpenApiDocument doc = new();
 		OpenApiSchemaReference schemaRef = new(SchemaConstants.SystemString, doc, null);
 
+		// Act
 		IOpenApiSchema result = OpenApiSchemaHelper.InlinePrimitiveTypeReference(schemaRef, doc);
+		OpenApiSchemaReference otherRef = new("MyNamespace.Foo", doc, null);
+		IOpenApiSchema otherResult = OpenApiSchemaHelper.InlinePrimitiveTypeReference(otherRef, doc);
 
+		// Assert
 		result.ShouldBeOfType<OpenApiSchema>();
 		OpenApiSchema s = (OpenApiSchema)result;
 		s.Type.ShouldBe(JsonSchemaType.String);
-
-		// Non-system type should be returned as the same reference
-		OpenApiSchemaReference otherRef = new("MyNamespace.Foo", doc, null);
-		IOpenApiSchema otherResult = OpenApiSchemaHelper.InlinePrimitiveTypeReference(otherRef, doc);
 		otherResult.ShouldBeSameAs(otherRef);
 	}
 
 	[Fact]
 	public void IsEnumSchemaReference_FindsReferencedEnum()
 	{
+		// Arrange
 		OpenApiDocument doc = new()
 		{
 			Components = new OpenApiComponents
@@ -118,8 +137,10 @@ public class OpenApiSchemaHelperTests
 
 		OpenApiSchemaReference schemaRef = new("MyEnum", doc, null);
 
+		// Act
 		bool result = OpenApiSchemaHelper.IsEnumSchemaReference(schemaRef, doc);
 
+		// Assert
 		result.ShouldBeTrue();
 	}
 
@@ -158,10 +179,13 @@ public class OpenApiSchemaHelperTests
 	[Fact]
 	public void EnrichSchemaWithEnumValues_AddsExtensionsAndDescription()
 	{
+		// Arrange
 		OpenApiSchema schema = new();
 
+		// Act
 		OpenApiSchemaHelper.EnrichSchemaWithEnumValues(schema, typeof(TestEnum), forStringSchema: false);
 
+		// Assert
 		schema.Extensions.ShouldNotBeNull();
 		schema.Extensions.ContainsKey(SchemaConstants.EnumExtension).ShouldBeTrue();
 		schema.Extensions.ContainsKey(SchemaConstants.EnumVarNamesExtension).ShouldBeTrue();
