1781c41110f3a9f69f5548d132f7404fdd4a15df
Ieuan Walker <Ieuan.Walker007@gmail.com>
2026-01-15 11:27:41 +0000

    Add unit tests for SchemaConstants utility methods
    Added OpenApiConstantsTests to cover SchemaConstants methods: IsSystemType, IsNullableType, IsCollectionType, IsDictionaryType, and ArraySuffix constant. Tests use xUnit theories to validate various input scenarios and expected results.

---
+++ tests/IeuanWalker.MinimalApi.Endpoints.Tests/OpenApiDocumentTransformers/RequestPropertyEnhancer/Core/OpenApiConstantsTests.cs
@@ -0,0 +1,58 @@
+﻿using IeuanWalker.MinimalApi.Endpoints.OpenApiDocumentTransformers.RequestPropertyEnhancer.Core;
+
+namespace IeuanWalker.MinimalApi.Endpoints.Tests.OpenApiDocumentTransformers.RequestPropertyEnhancer.Core;
+
+public class OpenApiConstantsTests
+{
+	[Theory]
+	[InlineData("System.String", true)]
+	[InlineData("System.Int32", true)]
+	[InlineData("MyNamespace.CustomType", false)]
+	public void IsSystemType_Works(string input, bool expected)
+	{
+		bool result = SchemaConstants.IsSystemType(input);
+
+		result.ShouldBe(expected);
+	}
+
+	[Theory]
+	[InlineData("System.Nullable`1[System.Int32]", true)]
+	[InlineData("System.Nullable`1[MyNamespace.Custom]", true)]
+	[InlineData("System.Int32", false)]
+	public void IsNullableType_Works(string input, bool expected)
+	{
+		bool result = SchemaConstants.IsNullableType(input);
+
+		result.ShouldBe(expected);
+	}
+
+	[Theory]
+	[InlineData("MyNamespace.Foo[]", true)]
+	[InlineData("System.Collections.Generic.List`1[System.String]", true)]
+	[InlineData("System.Collections.Generic.IEnumerable`1[System.String]", true)]
+	[InlineData("System.String", false)]
+	public void IsCollectionType_Works(string input, bool expected)
+	{
+		bool result = SchemaConstants.IsCollectionType(input);
+
+		result.ShouldBe(expected);
+	}
+
+	[Theory]
+	[InlineData("System.Collections.Generic.IDictionary`2[System.String,System.Int32]", true)]
+	[InlineData("System.Collections.Generic.Dictionary`2[System.String,System.Int32]", true)]
+	[InlineData("System.Collections.Concurrent.ConcurrentDictionary`2[System.String,System.Int32]", true)]
+	[InlineData("System.Collections.Generic.List`1[System.String]", false)]
+	public void IsDictionaryType_Works(string input, bool expected)
+	{
+		bool result = SchemaConstants.IsDictionaryType(input);
+
+		result.ShouldBe(expected);
+	}
+
+	[Fact]
+	public void ArraySuffix_Constant_IsBracketPair()
+	{
+		SchemaConstants.ArraySuffix.ShouldBe("[]");
+	}
+}
