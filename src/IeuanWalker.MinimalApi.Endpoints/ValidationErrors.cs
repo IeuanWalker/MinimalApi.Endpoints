1f8210207fb4ef3ac62c0f17519a27159e65cf7c
Ieuan Walker <Ieuan.Walker007@gmail.com>
2026-01-26 19:23:18 +0000

    Add ValidationErrors<T> for minimal API validation handling
    Introduced the ValidationErrors<T> class to manage and collect validation errors in ASP.NET Core minimal APIs. This class supports adding errors by property (via expressions or string keys), checking for errors, and converting them to standardized HTTP problem responses. Includes helper methods for property path extraction and example usage in exception handling. Also added an unrelated placeholder Class1.

---
+++ src/IeuanWalker.MinimalApi.Endpoints/ValidationErrors.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq.Expressions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace IeuanWalker.MinimalApi.Endpoints;
+
+public sealed class ValidationErrors<T>
+{
+	readonly Dictionary<string, List<string>> _errors = [];
+
+	public bool HasErrors()
+	{
+		return _errors.Count != 0;
+	}
+
+	public ValidationErrors<T> Add(Expression<Func<T, object>> property, params string[] messages)
+	{
+		string name = GetPropertyPath(property.Body);
+		return Add(name, messages);
+	}
+
+	public ValidationErrors<T> Add(string key, params string[] messages)
+	{
+		if (!_errors.TryGetValue(key, out List<string>? list))
+		{
+			list = [];
+			_errors[key] = list;
+		}
+		list.AddRange(messages);
+		return this;
+	}
+
+	public HttpValidationProblemDetails ToProblemDetails()
+	{
+		Dictionary<string, string[]> errorDict = _errors.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.ToArray());
+		return new HttpValidationProblemDetails(errorDict);
+	}
+
+	public ProblemHttpResult ToProblemResponse()
+	{
+		return TypedResults.Problem(ToProblemDetails());
+	}
+
+	static string GetPropertyPath(Expression expression)
+	{
+		Stack<string> members = new();
+		while (expression is MemberExpression memberExpr)
+		{
+			members.Push(memberExpr.Member.Name);
+			expression = memberExpr.Expression!;
+		}
+		if (expression is ParameterExpression)
+		{
+			return string.Join(".", members);
+		}
+
+		if (expression is UnaryExpression unary && unary.Operand is MemberExpression)
+		{
+			return GetPropertyPath(unary.Operand);
+		}
+
+		if (expression is MethodCallExpression call && call.Method.Name == "get_Item")
+		{
+			members.Push($"{((MemberExpression)call.Object!).Member.Name}[{call.Arguments[0]}]");
+			if (call.Object is MemberExpression)
+			{
+				return string.Join(".", members);
+			}
+		}
+
+		throw new ArgumentException("Expression must select a (possibly nested) property, e.g. x => x.Prop1.Prop2");
+	}
+}
+
+//ProblemHttpResult
+
+//	catch(InvalidUprnException)
+//		{
+//			return new ValidationErrors<RequestModel>()
+//				.Add(x => x.Uprn!, "Invalid UPRN")
+//				.ToProblemResponse();
+//		}
+//		catch(InvalidEastingNorthingException)
+//		{
+//	return new ValidationErrors<RequestModel>()
+//		.Add(x => x.Easting!, "Invalid easting")
+//		.Add(x => x.Northing!, "Invalid northing")
+//		.ToProblemResponse();
+//}
