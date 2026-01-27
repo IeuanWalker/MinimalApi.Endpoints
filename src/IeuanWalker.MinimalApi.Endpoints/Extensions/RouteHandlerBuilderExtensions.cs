34d1aebaf5b13b5fb707cc2fda656d3660cc6fa4
Ieuan Walker <Ieuan.Walker007@gmail.com>
2026-01-27 09:43:39 +0000

    Add RequireRoles extension for RouteHandlerBuilder
    Introduced RouteHandlerBuilderExtensions with a RequireRoles method,
    enabling role-based authorization on minimal API endpoints. Also
    suppressed namespace-folder structure warnings with pragma directives.

---
+++ src/IeuanWalker.MinimalApi.Endpoints/Extensions/RouteHandlerBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+#pragma warning disable IDE0130 // Namespace does not match folder structure
+namespace IeuanWalker.MinimalApi.Endpoints;
+#pragma warning restore IDE0130 // Namespace does not match folder structure
+
+public static class RouteHandlerBuilderExtensions
+{
+	extension(RouteHandlerBuilder source)
+	{
+		public RouteHandlerBuilder RequireRoles(params string[] roles)
+		{
+			return source.RequireAuthorization(p =>
+			{
+				p.RequireRole(roles);
+			});
+		}
+	}
+}
